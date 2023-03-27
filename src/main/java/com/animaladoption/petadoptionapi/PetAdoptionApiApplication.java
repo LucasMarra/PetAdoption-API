563adcea0bd32ebfd72a6b5f49be79cb0666244e
lucasmarra <lucasvmarra@gmail.com>
2023-03-27 20:56:27 -0300

    first commit

---
+++ src/main/java/com/animaladoption/petadoptionapi/PetAdoptionApiApplication.java
@@ -0,0 +1,13 @@
+package com.animaladoption.petadoptionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetAdoptionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetAdoptionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/animaladoption/petadoptionapi/PetAdoptionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animaladoption.petadoptionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetAdoptionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
